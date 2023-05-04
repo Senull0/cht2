c6dba7651f81193f88e4bc80728021b2149deb51
Senull0 <fnull@tuta.io>
2023-05-04 23:38:22 +0300

    in commit

---
+++ src/Avatar.jsx
@@ -0,0 +1,26 @@
+// Avatar generator component
+export default function Avatar({userId,username,online}) {
+    const colors = ['bg-teal-300', 'bg-red-300',
+                    'bg-green-300', 'bg-purple-300',
+                    'bg-blue-300', 'bg-yellow-300',
+                    'bg-orange-300', 'bg-pink-300', 'bg-fuchsia-300', 'bg-rose-300'];
+
+                    
+    const userIdBase10 = parseInt(userId.substring(10), 16);
+    const colorIndex = userIdBase10 % colors.length;
+    const color = colors[colorIndex];
+    
+    
+    return (
+      <div className={"w-10 h-10 relative rounded-full flex items-center "+color}>
+        <div className="text-center w-full opacity-70">{username.substring(0, 2)}</div>
+        {online && (
+          <div className="absolute w-3 h-3 bg-green-400 bottom-0 right-0 rounded-full border border-white"></div>
+        )}
+        {!online && (
+          <div className="absolute w-3 h-3 bg-gray-400 bottom-0 right-0 rounded-full border border-white"></div>
+        )}
+      </div>
+    );
+  }
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  darkMode: 'class',
+  content: [
+    "./src/*.jsx"
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'xs': '.55rem',
+      }
+  },
+  plugins: [],
+}
+}
